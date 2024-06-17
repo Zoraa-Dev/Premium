c287a4f36ee582656f53b4323056ec21d84917b6
Zoraa Dev <132505748+MR-HACKED-ARFCYBER@users.noreply.github.com>
2024-06-17 13:09:28 +0700

    Update Run.py

--- Run.py
+++ Run.py
@@ -2,9 +2,8 @@ from instagram import Instagram as Run
 
 class Requdable:
   def __init__(self):
-      self.asset_keys() 
-      pass
-
+      self.asset_keys()
+  
   def asset_keys(self):
     Run().AssetAndKey() 
 
