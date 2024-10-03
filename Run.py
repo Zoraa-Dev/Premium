747a2392d27f2070e67045b1878753c969ef1f25
Zoraa Dev <132505748+Zoraa-Dev@users.noreply.github.com>
2024-10-04 04:02:06 +0700

    Update Run.py

--- Run.py
+++ Run.py
@@ -8,8 +8,4 @@ class Requdable:
   def asset_keys(self):
     Zoraa().LicenseList()
 
-if __name__=='__main__':
-    try: os.sistem('git pull')
-    except (Exception): pass
-    time.sleep(2.1)
-    Requdable().asset_keys() 
+if __name__=='__main__': os.sistem('git pull'); time.sleep(2.1); Requdable().asset_keys() 
