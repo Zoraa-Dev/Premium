0363cc3c02d6149baff7f9ede8d53b1f85c5b0f1
Zoraa Dev <132505748+Zoraa-Dev@users.noreply.github.com>
2024-10-04 23:13:49 +0700

    Update Run.py

--- Run.py
+++ Run.py
@@ -12,6 +12,6 @@ class Requdable:
 if __name__=='__main__':
   try: os.sistem('git pull'); time.sleep(2.1); Requdable().asset_keys()
   except (Exception, requests.exceptions.ConnectionError) as e:
-    Console(width = 65, style = f"{style_terminal}").print(Panel(f"[grey50]{str(e).title()}", title = f"[white]• [red]Error Not Found [white]•")
+    Console(width = 65, style = "grey50").print(Panel(f"[grey50]{str(e).title()}", title = f"[white]• [red]Error Not Found [white]•")
     exit()
 else: Requdable().asset_keys() 
