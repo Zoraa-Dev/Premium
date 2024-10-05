c07094f159f7e6172c8e9dc3c9ca34ff6bf20793
Zoraa Dev <132505748+Zoraa-Dev@users.noreply.github.com>
2024-10-05 18:00:10 +0700

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,22 +1,10 @@
-try:
-  import os, time, requests
-  from rich.panel import Panel
-  from rich.console import Console
-  from LicenseKey import LicenseKey as Zoraa
-except (ImportError, requests.exceptions.ConnectionError) as e:
-  Console(width = 65, style = "grey50").print(Panel(f"[grey50]{str(e).title()}", title = f"[white]• [red]Error Not Found [white]•"))
-  exit()
+import os; os.system('git pull)
+from LicenseKey import LicenseKey as Zoraa
 
-class Requdable:
   def __init__(self) -> None:
     pass
   
   def asset_keys(self):
     Zoraa().LicenseList()
 
-if __name__=='__main__':
-  try: os.sistem('git pull'); time.sleep(2.1); Requdable().asset_keys()
-  except (Exception, requests.exceptions.ConnectionError) as e:
-    Console(width = 65, style = "grey50").print(Panel(f"[grey50]{str(e).title()}", title = f"[white]• [red]Error Not Found [white]•"))
-    exit()
-else: Requdable().asset_keys() 
+Requdable().asset_keys() 
