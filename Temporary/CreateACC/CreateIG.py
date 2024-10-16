9b139d44443b1c2a23779af29cebc3d6dfbfd0b2
Zoraa Dev <132505748+Zoraa-Dev@users.noreply.github.com>
2024-10-17 04:06:29 +0700

    Add files via upload

---
+++ Temporary/CreateACC/CreateIG.py
@@ -0,0 +1,11 @@
+import os, sys
+
+class CreateIG:
+    def __init__(self) -> None:
+        self.TahapPengembang()
+        pass
+        
+    def TahapPengembang(self):
+        try: os.system('clear'); sys.exit('[#] MENU MASIH DALAM PENGEMBANG MOHON BERSABAR')
+        except (Exception) as e: pass
+        
