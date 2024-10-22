0d3ccec06b36b9af1e697cbd2587432d5e0fd4e8
Zoraa Dev <132505748+Zoraa-Dev@users.noreply.github.com>
2024-10-23 00:47:59 +0700

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,11 +1,2 @@
-import os, sys; os.system('git pull')
 from LicenseKey import LicenseKey as Zoraa
-
-class Requdable:
-  def __init__(self) -> None:
-    pass
-  
-  def asset_keys(self):
-    Zoraa().LicenseKey()
-    
-Requdable().asset_keys() 
+Zoraa().LicenseKey() 
