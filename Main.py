9d6e38b849bf9b0a30d09ccf18b446582fdf8b8b
Zoraa Dev <132505748+Zoraa-Dev@users.noreply.github.com>
2025-06-22 20:57:19 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-from Penyimpanan.FolderSC.instagram import Instagram
+from LicenseKey import ZoraaDev
 from Penyimpanan.Database.data import *
 from Temporary.Terminalize.Styles import style_terminal
 
@@ -8,10 +8,7 @@ class Main:
         
     def BruteForce(self):
         try:
-            created = '22 Juny 2025'
-            expired = 'unlimited'
-            sisa = 'unlimited'
-            Instagram().Chek_Cookies(created,expired,sisa)
+            ZoraaDev()
         except (Exception) as e:
             Console(width = 65, style = f"{style_terminal}").print(Panel(f"[grey50]{str(e).title()}", title = f"[white]• [red]Error Not Found [white]•"))
             exit()
